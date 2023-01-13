c615d4f993373b1f68047366eb214b839b222ff4
Khrom92 <Khromov992@gmail.com>
2023-01-13 11:59:46 +0700

    change port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  await app.listen(8000);
+  await app.listen(config.api.port);
 }
 bootstrap();
